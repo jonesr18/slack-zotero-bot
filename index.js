9b3e31832dca19bbcba6c6dd756f71d3614b2373
Ross Jones <jonesr18@gmail.com>
2026-03-18 17:42:08 -0700

    Accept all network interfaces

--- index.js
+++ index.js
@@ -129,4 +129,4 @@ app.post("/slack/events", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Bot listening on :${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Bot listening on :${PORT}`));
